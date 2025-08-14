83ebca9db344ac33964d89b7ffe53276c46272fb
mr-biggles <92831318+mr-biggles@users.noreply.github.com>
2025-08-14 15:57:57 +0200

    Update init_file.py

--- SocialThreadOpener/init_file.py
+++ SocialThreadOpener/init_file.py
@@ -1,4 +1,7 @@
 from .socialthreadopener import SocialThreadOpener
 
+__red_end_user_data_statement__ = "This cog does not persistently store data about users."
+
 async def setup(bot):
-    await bot.add_cog(SocialThreadOpener(bot))
+    cog = SocialThreadOpener(bot)
+    await bot.add_cog(cog)
