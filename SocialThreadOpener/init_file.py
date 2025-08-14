b8c78f0d570e8b5d1c650db9de9010bc4ddcd363
mr-biggles <92831318+mr-biggles@users.noreply.github.com>
2025-08-14 16:00:36 +0200

    Delete SocialThreadOpener directory

--- SocialThreadOpener/init_file.py
+++
@@ -1,7 +0,0 @@
-from .socialthreadopener import SocialThreadOpener
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-async def setup(bot):
-    cog = SocialThreadOpener(bot)
-    await bot.add_cog(cog)
